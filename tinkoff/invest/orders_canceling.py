f0ddd557620c308876f5f75df97563080f396b57
Ruslan Sirazhetdinov <ruslansir@icloud.com>
2022-01-28 23:49:21 +0500

    flint

--- tinkoff/invest/orders_canceling.py
+++ tinkoff/invest/orders_canceling.py
@@ -19,8 +19,14 @@ class OrdersCanceler:
     def cancel_all(self) -> None:
         orders_response = self._orders_service.get_orders(account_id=self._account_id)
         for order in orders_response.orders:
-            self._orders_service.cancel_order(account_id=self._account_id, order_id=order.order_id)
+            self._orders_service.cancel_order(
+                account_id=self._account_id, order_id=order.order_id
+            )
 
-        stop_orders_response = self._stop_orders_service.get_stop_orders(account_id=self._account_id)
+        stop_orders_response = self._stop_orders_service.get_stop_orders(
+            account_id=self._account_id
+        )
         for stop_order in stop_orders_response.stop_orders:
-            self._stop_orders_service.cancel_stop_order(account_id=self._account_id, stop_order_id=stop_order.stop_order_id)
+            self._stop_orders_service.cancel_stop_order(
+                account_id=self._account_id, stop_order_id=stop_order.stop_order_id
+            )
