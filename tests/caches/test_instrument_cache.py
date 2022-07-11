b9701e42911796d8faae0c80ead5f0d045873c02
Ruslan Sirazhetdinov <ruslansir@icloud.com>
2022-07-12 00:48:33 +0500

    fix: format

--- tests/caches/test_instrument_cache.py
+++ tests/caches/test_instrument_cache.py
@@ -1,18 +1,32 @@
-import logging
 import uuid
 from pprint import pprint
-from typing import Iterator, List, Dict, Type
+from typing import Dict, Iterator, Type
 
 import pytest
 
-from tinkoff.invest import Client, InstrumentIdType, EtfsResponse, Etf, SharesResponse, \
-    Share, BondsResponse, Bond, CurrenciesResponse, Currency, FuturesResponse, Future, \
-    InstrumentResponse, EtfResponse, ShareResponse, BondResponse, CurrencyResponse, \
-    FutureResponse
+from tinkoff.invest import (
+    Bond,
+    BondResponse,
+    BondsResponse,
+    Client,
+    CurrenciesResponse,
+    Currency,
+    CurrencyResponse,
+    Etf,
+    EtfResponse,
+    EtfsResponse,
+    Future,
+    FutureResponse,
+    FuturesResponse,
+    InstrumentIdType,
+    Share,
+    ShareResponse,
+    SharesResponse,
+)
 from tinkoff.invest.caching.instruments_cache.instruments_cache import InstrumentsCache
 from tinkoff.invest.caching.instruments_cache.models import InstrumentsResponse
 from tinkoff.invest.caching.instruments_cache.settings import InstrumentsCacheSettings
-from tinkoff.invest.services import Services, InstrumentsService
+from tinkoff.invest.services import InstrumentsService, Services
 
 
 def uid() -> str:
@@ -35,12 +49,15 @@ def gen_meta_ids() -> Dict[str, str]:
 
 
 def gen_instruments(type_: Type, instrument_count: int = 10):
-    return [type_(name=f'{type_.__name__}_{i}', **gen_meta_ids()) for i in
-            range(instrument_count)]
+    return [
+        type_(name=f"{type_.__name__}_{i}", **gen_meta_ids())
+        for i in range(instrument_count)
+    ]
 
 
-def gen_instruments_response(response_type: Type, type_: Type,
-                             instrument_count: int = 10):
+def gen_instruments_response(
+    response_type: Type, type_: Type, instrument_count: int = 10
+):
     return response_type(instruments=gen_instruments(type_, instrument_count))
 
 
@@ -64,17 +81,17 @@ def mock_get_by(instrument_response: InstrumentsResponse, response_type):
     }
 
     def _mock_get_by(
-            *,
-            id_type: InstrumentIdType = InstrumentIdType(0),
-            class_code: str = "",
-            id: str = "",
+        *,
+        id_type: InstrumentIdType = InstrumentIdType(0),
+        class_code: str = "",
+        id: str = "",
     ):
         get_id = type_to_field_extractor[id_type]
 
         def filter_(instrument):
             return get_id(instrument) == id and instrument.class_code == class_code
 
-        found_instrument, = filter(filter_, instrument_response.instruments)
+        (found_instrument,) = filter(filter_, instrument_response.instruments)
         return response_type(instrument=found_instrument)
 
     return _mock_get_by
@@ -82,47 +99,56 @@ def mock_get_by(instrument_response: InstrumentsResponse, response_type):
 
 @pytest.fixture()
 def mock_instruments_service(
-        real_services: Services,
-        mocker,
-        instrument_map,
+    real_services: Services,
+    mocker,
+    instrument_map,
 ) -> Services:
     real_services.instruments: InstrumentsService = mocker.Mock(
         wraps=real_services.instruments,
     )
 
-    real_services.instruments.etfs.__name__ = 'etfs'
+    real_services.instruments.etfs.__name__ = "etfs"
     real_services.instruments.etfs.return_value = instrument_map[Etf]
     real_services.instruments.etf_by = mock_get_by(instrument_map[Etf], EtfResponse)
 
-    real_services.instruments.shares.__name__ = 'shares'
+    real_services.instruments.shares.__name__ = "shares"
     real_services.instruments.shares.return_value = instrument_map[Share]
-    real_services.instruments.share_by = mock_get_by(instrument_map[Share], ShareResponse)
+    real_services.instruments.share_by = mock_get_by(
+        instrument_map[Share], ShareResponse
+    )
 
-    real_services.instruments.bonds.__name__ = 'bonds'
+    real_services.instruments.bonds.__name__ = "bonds"
     real_services.instruments.bonds.return_value = instrument_map[Bond]
     real_services.instruments.bond_by = mock_get_by(instrument_map[Bond], BondResponse)
 
-    real_services.instruments.currencies.__name__ = 'currencies'
+    real_services.instruments.currencies.__name__ = "currencies"
     real_services.instruments.currencies.return_value = instrument_map[Currency]
-    real_services.instruments.currency_by = mock_get_by(instrument_map[Currency], CurrencyResponse)
+    real_services.instruments.currency_by = mock_get_by(
+        instrument_map[Currency], CurrencyResponse
+    )
 
-    real_services.instruments.futures.__name__ = 'futures'
+    real_services.instruments.futures.__name__ = "futures"
     real_services.instruments.futures.return_value = instrument_map[Future]
-    real_services.instruments.future_by = mock_get_by(instrument_map[Future], FutureResponse)
+    real_services.instruments.future_by = mock_get_by(
+        instrument_map[Future], FutureResponse
+    )
 
     return real_services
 
 
 @pytest.fixture()
 def mocked_services(
-        real_services: Services,
-        mock_instruments_service,
+    real_services: Services,
+    mock_instruments_service,
 ) -> Services:
     return real_services
 
 
 class TestInstrumentCache:
-    def test_a(self, mocked_services: Services, ):
+    def test_a(
+        self,
+        mocked_services: Services,
+    ):
         inst = mocked_services.instruments.etfs().instruments[0]
 
         from_server = mocked_services.instruments.etf_by(
