4cee2970ae91edfddf586bda17c57d01f4ccb96d
Ruslan Sirazhetdinov <ruslansir@icloud.com>
2022-08-26 00:34:21 +0300

    fix: lint

--- examples/get_operations_by_cursor.py
+++ examples/get_operations_by_cursor.py
@@ -1,15 +1,15 @@
 import os
+
 from tinkoff.invest import Client, GetOperationsByCursorRequest
 
-
-token = os.environ['INVEST_TOKEN']
+token = os.environ["INVEST_TOKEN"]
 
 
 with Client(token) as client:
     accounts = client.users.get_accounts()
     account_id = accounts.accounts[0].id
 
-    def get_request(cursor=''):
+    def get_request(cursor=""):
         return GetOperationsByCursorRequest(
             account_id=account_id,
             instrument_id="BBG004730N88",
