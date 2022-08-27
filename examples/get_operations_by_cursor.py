6c4385d73cdae3176fc306431c639580c03a95ac
Ruslan Sirazhetdinov <49832869+irusland@users.noreply.github.com>
2022-08-27 21:14:42 +0300

    fix: fix get operations by cursor (#109)

---
+++ examples/get_operations_by_cursor.py
@@ -0,0 +1,25 @@
+import os
+
+from tinkoff.invest import Client, GetOperationsByCursorRequest
+
+token = os.environ["INVEST_TOKEN"]
+
+
+with Client(token) as client:
+    accounts = client.users.get_accounts()
+    account_id = accounts.accounts[0].id
+
+    def get_request(cursor=""):
+        return GetOperationsByCursorRequest(
+            account_id=account_id,
+            instrument_id="BBG004730N88",
+            cursor=cursor,
+            limit=1,
+        )
+
+    operations = client.operations.get_operations_by_cursor(get_request())
+    print(operations)
+    while operations.has_next:
+        request = get_request(cursor=operations.next_cursor)
+        operations = client.operations.get_operations_by_cursor(request)
+        print(operations)
