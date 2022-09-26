16ff28826786b8230541d3fe9c5353d35fb29112
Ruslan Sirazhetdinov <ruslansir@icloud.com>
2022-09-26 04:03:50 +0300

    Revert "Revert "fix: rm allow_dups""
    This reverts commit 94c622d530118b818a2f06a03505f3b6dc3901b2.

--- tinkoff/invest/services.py
+++ tinkoff/invest/services.py
@@ -373,18 +373,7 @@ class Services(ICandleGetter):
         to: Optional[datetime] = None,
         interval: CandleInterval = CandleInterval(0),
         figi: str = "",
-        allow_candle_duplicates: bool = True,
     ) -> Generator[HistoricCandle, None, None]:
-        """
-        Запрос свечей за определенный интервал.
-
-        Args:
-            from_ (datetime or None): Дата с которой загружаются котировки
-            to (Optional[datetime]): Дата до которой загружаются котировки
-            figi (str): FIGI инструмента
-            interval (CandleInterval): Интервал свечей
-            allow_candle_duplicates (bool): Разрешить свечам дубилроваться
-        """
         to = to or now()
 
         previous_candles = set()
@@ -396,15 +385,12 @@ class Services(ICandleGetter):
                 to=current_to,
             )
 
-            if allow_candle_duplicates:
-                yield from candles_response.candles
-            else:
-                for candle in candles_response.candles:
-                    if candle not in previous_candles:
-                        yield candle
-                        previous_candles.add(candle)
+            for candle in candles_response.candles:
+                if candle not in previous_candles:
+                    yield candle
+                    previous_candles.add(candle)
 
-                previous_candles = set(candles_response.candles)
+            previous_candles = set(candles_response.candles)
 
 
 class InstrumentsService(_grpc_helpers.Service):
