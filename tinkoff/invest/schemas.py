aff313e06ef4de1c685265187c83863995fd0b37
Ruslan Sirazhetdinov <ruslansir@icloud.com>
2023-01-14 10:44:10 +0300

    fix: instrument_id for subscribe_last_price_request

--- tinkoff/invest/schemas.py
+++ tinkoff/invest/schemas.py
@@ -1321,7 +1321,7 @@ class SubscribeLastPriceRequest(_grpc_helpers.Message):
 @dataclass(eq=False, repr=True)
 class LastPriceInstrument(_grpc_helpers.Message):
     figi: str = _grpc_helpers.string_field(1)
-    instrument_uid: str = _grpc_helpers.string_field(2)
+    instrument_id: str = _grpc_helpers.string_field(2)
 
 
 @dataclass(eq=False, repr=True)
