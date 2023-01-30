28cf0d9a94d45138aff0107fc849122007adfe59
Oleg Shpagin <info@wiseplat.org>
2023-01-30 14:54:36 +0300

    feat: add new example for cancel only stop orders

--- examples/wiseplat_cancel_all_stop_orders.py
+++ examples/wiseplat_cancel_all_stop_orders.py
@@ -1,16 +1,18 @@
+"""Example - How to cancel all stop orders."""
 import os
+import logging
 
 from tinkoff.invest import Client
 from tinkoff.invest.exceptions import InvestError
 
-import logging
+TOKEN = os.environ["INVEST_TOKEN"]
+
 logging.basicConfig(format="%(asctime)s %(levelname)s:%(message)s", level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-TOKEN = os.environ["INVEST_TOKEN"]
-
 
 def main():
+    """Example - How to cancel all stop orders."""
     with Client(TOKEN) as client:
         response = client.users.get_accounts()
         account, *_ = response.accounts
@@ -20,11 +22,12 @@ def main():
             stop_orders_response = client.stop_orders.get_stop_orders(account_id=account_id)
             logger.info("Stop Orders: %s", stop_orders_response)
             for stop_order in stop_orders_response.stop_orders:
-                client.stop_orders.cancel_stop_order(account_id=account_id, stop_order_id=stop_order.stop_order_id)
+                client.stop_orders.cancel_stop_order(account_id=account_id,
+                                                     stop_order_id=stop_order.stop_order_id)
                 logger.info("Stop Order: %s was canceled.", stop_order.stop_order_id)
             logger.info("Orders: %s", client.stop_orders.get_stop_orders(account_id=account_id))
         except InvestError as error:
-            logger.error(f'Failed to cancel all orders. Error: {error}')
+            logger.error('Failed to cancel all orders. Error: %s', error)
 
 
 if __name__ == "__main__":
