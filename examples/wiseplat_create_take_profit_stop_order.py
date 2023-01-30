c9112cf9308cb52d087e2e6293190203c456796e
Oleg Shpagin <info@wiseplat.org>
2023-01-30 14:51:38 +0300

    feat: add new example for takeprofit stop order

--- examples/wiseplat_create_take_profit_stop_order.py
+++ examples/wiseplat_create_take_profit_stop_order.py
@@ -1,19 +1,22 @@
+"""Example - How to create takeprofit buy order."""
 import os
+
+from decimal import Decimal
+import logging
 
 from tinkoff.invest import InstrumentIdType
 from tinkoff.invest import Client, StopOrderDirection, StopOrderType, StopOrderExpirationType
 from tinkoff.invest.utils import quotation_to_decimal, decimal_to_quotation
 from tinkoff.invest.exceptions import InvestError
-from decimal import Decimal
-
-import logging
-logging.basicConfig(format="%(asctime)s %(levelname)s:%(message)s", level=logging.DEBUG)
-logger = logging.getLogger(__name__)
 
 TOKEN = os.environ["INVEST_TOKEN"]
 
+logging.basicConfig(format="%(asctime)s %(levelname)s:%(message)s", level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
 
 def main():
+    """Example - How to create takeprofit buy order."""
     with Client(TOKEN) as client:
         response = client.users.get_accounts()
         account, *_ = response.accounts
@@ -21,14 +24,15 @@ def main():
         logger.info("Orders: %s", client.orders.get_orders(account_id=account_id))
 
         # figi="BBG004730N88"  # SBER
-        figi="BBG004730ZJ9"  # VTBR
+        figi = "BBG004730ZJ9"  # VTBR
 
         # getting the last price for instrument
         last_price = client.market_data.get_last_prices(figi=[figi]).last_prices[0].price
         last_price = quotation_to_decimal(last_price)
         print(f'figi, last price = {last_price}')
 
-        # setting the percentage by which the takeprofit stop order should be set below the last price
+        # setting the percentage by which the takeprofit stop order
+        # should be set below the last price
         percent_down = 5
 
         # calculation of the price for takeprofit stop order
@@ -37,14 +41,17 @@ def main():
 
         # getting the min price increment and the number of digits after point
         min_price_increment = client.instruments.get_instrument_by(
-            id_type=InstrumentIdType.INSTRUMENT_ID_TYPE_FIGI, id=figi).instrument.min_price_increment
+            id_type=InstrumentIdType.INSTRUMENT_ID_TYPE_FIGI,
+            id=figi).instrument.min_price_increment
         number_digits_after_point = 9 - len(str(min_price_increment.nano)) + 1
         min_price_increment = quotation_to_decimal(min_price_increment)
-        print(f'min_price_increment = {min_price_increment}, number_digits_after_point={number_digits_after_point}')
+        print(f'min_price_increment = {min_price_increment}, '
+              f'number_digits_after_point={number_digits_after_point}')
 
         # calculation of the price for instrument which is divisible to min price increment
         calculated_price = round(calculated_price / min_price_increment) * min_price_increment
-        print(f'let\'s send stop order at price = {calculated_price:.{number_digits_after_point}f} divisible to '
+        print(f'let\'s send stop order at price = '
+              f'{calculated_price:.{number_digits_after_point}f} divisible to '
               f'min price increment {min_price_increment}')
 
         # creating takeprofit buy order
@@ -63,7 +70,7 @@ def main():
             print(response)
             print("stop_order_id=", response.stop_order_id)
         except InvestError as error:
-            logger.error(f'Posting trade takeprofit order failed. Exception: {error}')
+            logger.error('Posting trade takeprofit order failed. Exception: %s', error)
 
 
 if __name__ == "__main__":
