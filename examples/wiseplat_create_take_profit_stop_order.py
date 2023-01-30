829c3e7b0116b9146ee8321040bf574d6730631c
Oleg Shpagin <info@wiseplat.org>
2023-01-30 13:32:28 +0300

    feat: add new example for takeprofit stop order

---
+++ examples/wiseplat_create_take_profit_stop_order.py
@@ -0,0 +1,70 @@
+import os
+
+from tinkoff.invest import InstrumentIdType
+from tinkoff.invest import Client, StopOrderDirection, StopOrderType, StopOrderExpirationType
+from tinkoff.invest.utils import quotation_to_decimal, decimal_to_quotation
+from tinkoff.invest.exceptions import InvestError
+from decimal import Decimal
+
+import logging
+logging.basicConfig(format="%(asctime)s %(levelname)s:%(message)s", level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+TOKEN = os.environ["INVEST_TOKEN"]
+
+
+def main():
+    with Client(TOKEN) as client:
+        response = client.users.get_accounts()
+        account, *_ = response.accounts
+        account_id = account.id
+        logger.info("Orders: %s", client.orders.get_orders(account_id=account_id))
+
+        # figi="BBG004730N88"  # SBER
+        figi="BBG004730ZJ9"  # VTBR
+
+        # getting the last price for instrument
+        last_price = client.market_data.get_last_prices(figi=[figi]).last_prices[0].price
+        last_price = quotation_to_decimal(last_price)
+        print(f'figi, last price = {last_price}')
+
+        # setting the percentage by which the takeprofit stop order should be set below the last price
+        percent_down = 5
+
+        # calculation of the price for takeprofit stop order
+        calculated_price = last_price - last_price * Decimal(percent_down / 100)
+        print(f'calculated_price = {calculated_price}')
+
+        # getting the min price increment and the number of digits after point
+        min_price_increment = client.instruments.get_instrument_by(
+            id_type=InstrumentIdType.INSTRUMENT_ID_TYPE_FIGI, id=figi).instrument.min_price_increment
+        number_digits_after_point = 9 - len(str(min_price_increment.nano)) + 1
+        min_price_increment = quotation_to_decimal(min_price_increment)
+        print(f'min_price_increment = {min_price_increment}, number_digits_after_point={number_digits_after_point}')
+
+        # calculation of the price for instrument which is divisible to min price increment
+        calculated_price = round(calculated_price / min_price_increment) * min_price_increment
+        print(f'let\'s send stop order at price = {calculated_price:.{number_digits_after_point}f} divisible to '
+              f'min price increment {min_price_increment}')
+
+        # creating takeprofit buy order
+        try:
+            response = client.stop_orders.post_stop_order(
+                figi=figi,
+                quantity=1,
+                price=decimal_to_quotation(Decimal(calculated_price)),
+                stop_price=decimal_to_quotation(Decimal(calculated_price)),
+                direction=StopOrderDirection.STOP_ORDER_DIRECTION_BUY,
+                account_id=account_id,
+                expiration_type=StopOrderExpirationType.STOP_ORDER_EXPIRATION_TYPE_GOOD_TILL_CANCEL,
+                stop_order_type=StopOrderType.STOP_ORDER_TYPE_TAKE_PROFIT,
+                expire_date=None,
+            )
+            print(response)
+            print("stop_order_id=", response.stop_order_id)
+        except InvestError as error:
+            logger.error(f'Posting trade takeprofit order failed. Exception: {error}')
+
+
+if __name__ == "__main__":
+    main()
