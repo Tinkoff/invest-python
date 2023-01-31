e5ccb01dc8ef1677e3ba767b0e5477d03bb62354
Oleg Shpagin <info@wiseplat.org>
2023-01-31 11:35:09 +0300

    feat: add examples for takeprofit stop order and cancel only stop orders

--- examples/wiseplat_cancel_all_stop_orders.py
+++ examples/wiseplat_cancel_all_stop_orders.py
@@ -20,17 +20,19 @@ def main():
 
         try:
             stop_orders_response = client.stop_orders.get_stop_orders(
-                account_id=account_id)
+                account_id=account_id
+            )
             logger.info("Stop Orders: %s", stop_orders_response)
             for stop_order in stop_orders_response.stop_orders:
                 client.stop_orders.cancel_stop_order(
-                    account_id=account_id,
-                    stop_order_id=stop_order.stop_order_id)
+                    account_id=account_id, stop_order_id=stop_order.stop_order_id
+                )
                 logger.info("Stop Order: %s was canceled.", stop_order.stop_order_id)
-            logger.info("Orders: %s",
-                        client.stop_orders.get_stop_orders(account_id=account_id))
+            logger.info(
+                "Orders: %s", client.stop_orders.get_stop_orders(account_id=account_id)
+            )
         except InvestError as error:
-            logger.error('Failed to cancel all orders. Error: %s', error)
+            logger.error("Failed to cancel all orders. Error: %s", error)
 
 
 if __name__ == "__main__":
--- examples/wiseplat_create_take_profit_stop_order.py
+++ examples/wiseplat_create_take_profit_stop_order.py
@@ -30,10 +30,11 @@ def main():
         figi = "BBG004730ZJ9"  # BBG004730ZJ9 - VTBR / BBG004730N88 - SBER
 
         # getting the last price for instrument
-        last_price = client.market_data.get_last_prices(
-            figi=[figi]).last_prices[0].price
+        last_price = (
+            client.market_data.get_last_prices(figi=[figi]).last_prices[0].price
+        )
         last_price = quotation_to_decimal(last_price)
-        print(f'figi, last price = {last_price}')
+        print(f"figi, last price = {last_price}")
 
         # setting the percentage by which the takeprofit stop order
         # should be set below the last price
@@ -41,24 +42,29 @@ def main():
 
         # calculation of the price for takeprofit stop order
         calculated_price = last_price - last_price * Decimal(percent_down / 100)
-        print(f'calculated_price = {calculated_price}')
+        print(f"calculated_price = {calculated_price}")
 
         # getting the min price increment and the number of digits after point
         min_price_increment = client.instruments.get_instrument_by(
-            id_type=InstrumentIdType.INSTRUMENT_ID_TYPE_FIGI,
-            id=figi).instrument.min_price_increment
+            id_type=InstrumentIdType.INSTRUMENT_ID_TYPE_FIGI, id=figi
+        ).instrument.min_price_increment
         number_digits_after_point = 9 - len(str(min_price_increment.nano)) + 1
         min_price_increment = quotation_to_decimal(min_price_increment)
-        print(f'min_price_increment = {min_price_increment}, '
-              f'number_digits_after_point={number_digits_after_point}')
+        print(
+            f"min_price_increment = {min_price_increment}, "
+            f"number_digits_after_point={number_digits_after_point}"
+        )
 
         # calculation of the price for instrument which is
         # divisible to min price increment
-        calculated_price = round(
-            calculated_price / min_price_increment) * min_price_increment
-        print(f'let\'s send stop order at price = '
-              f'{calculated_price:.{number_digits_after_point}f} divisible to '
-              f'min price increment {min_price_increment}')
+        calculated_price = (
+            round(calculated_price / min_price_increment) * min_price_increment
+        )
+        print(
+            f"let's send stop order at price = "
+            f"{calculated_price:.{number_digits_after_point}f} divisible to "
+            f"min price increment {min_price_increment}"
+        )
 
         # creating takeprofit buy order
         stop_order_type = StopOrderType.STOP_ORDER_TYPE_TAKE_PROFIT
@@ -79,7 +85,7 @@ def main():
             print(response)
             print("stop_order_id=", response.stop_order_id)
         except InvestError as error:
-            logger.error('Posting trade takeprofit order failed. Exception: %s', error)
+            logger.error("Posting trade takeprofit order failed. Exception: %s", error)
 
 
 if __name__ == "__main__":
