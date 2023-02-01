a07c9f322aa001046514bcf84c336ca140702759
Oleg Shpagin <info@wiseplat.org>
2023-02-01 08:55:09 +0300

    docs(examples): add new example for how to get figi for ticker

---
+++ examples/wiseplat_get_figi_for_ticker.py
@@ -0,0 +1,67 @@
+"""Example - How to get figi by name of ticker."""
+import logging
+import os
+from pandas import DataFrame
+
+from tinkoff.invest import Client
+from tinkoff.invest.services import InstrumentsService
+from tinkoff.invest.utils import quotation_to_decimal
+
+TOKEN = os.environ["INVEST_TOKEN"]
+
+logging.basicConfig(format="%(asctime)s %(levelname)s:%(message)s", level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+def main():
+    """Example - How to get figi by name of ticker."""
+
+    ticker = "VTBR"  # "BRH3" "SBER" "VTBR"
+
+    with Client(TOKEN) as cl:
+        instruments: InstrumentsService = cl.instruments
+        l = []
+        for method in ["shares", "bonds", "etfs", "currencies", "futures"]:
+            for item in getattr(instruments, method)().instruments:
+                l.append(
+                    {
+                        "name": item.name,
+                        "ticker": item.ticker,
+                        "class_code": item.class_code,
+                        "figi": item.figi,
+                        "uid": item.uid,
+                        "type": method,
+                        "min_price_increment": quotation_to_decimal(
+                            item.min_price_increment
+                        ),
+                        "scale": 9 - len(str(item.min_price_increment.nano)) + 1,
+                        "lot": item.lot,
+                        "trading_status": item.__getattribute__(
+                            "trading_status"
+                        )._name_,
+                        "api_trade_available_flag": item.api_trade_available_flag,
+                        "currency": item.currency,
+                        "exchange": item.exchange,
+                        "buy_available_flag": item.buy_available_flag,
+                        "sell_available_flag": item.sell_available_flag,
+                        "short_enabled_flag": item.short_enabled_flag,
+                        "klong": quotation_to_decimal(item.klong),
+                        "kshort": quotation_to_decimal(item.kshort),
+                    }
+                )
+
+        df = DataFrame(l)
+
+        df = df[df["ticker"] == ticker]
+        if df.empty:
+            logger.error("There is no such ticker: %s", ticker)
+            return
+
+        figi = df["figi"].iloc[0]
+        print(f"\nTicker {ticker} have figi={figi}\n")
+        print(f"Additional info for this {ticker} ticker:")
+        print(df.iloc[0])
+
+
+if __name__ == "__main__":
+    main()
