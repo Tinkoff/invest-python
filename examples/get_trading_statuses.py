b643926479a3bbf8d7140e29400b42d2dbe67d9c
Ruslan Sirazhetdinov <r.sirazhetdinov@tinkoff.ru>
2023-02-28 08:36:08 +0300

    fix: add async_get_trading_statuses, add and fix example

--- examples/get_trading_statuses.py
+++ examples/get_trading_statuses.py
@@ -6,8 +6,5 @@ token = os.environ["INVEST_TOKEN"]
 
 
 with Client(token) as client:
-    accounts = client.users.get_accounts()
-    account_id = accounts.accounts[0].id
-
     statuses = client.market_data.get_trading_statuses(instrument_ids=["BBG004730N88"])
     print(statuses)
