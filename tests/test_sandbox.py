b5fd7afecfca73b425b13bf7de6b6626d51a16e0
Ruslan Sirazhetdinov <ruslansir@icloud.com>
2023-04-05 23:04:00 +0300

    feat: rewrite tests

--- tests/test_sandbox.py
+++ tests/test_sandbox.py
@@ -21,14 +21,14 @@ from tinkoff.invest.sandbox.client import SandboxClient
 @pytest.fixture()
 def sandbox_service():
     with SandboxClient(token=os.environ["INVEST_SANDBOX_TOKEN"]) as client:
-        yield client.sandbox
+        yield client
 
 
 @pytest.fixture()
 def account_id(sandbox_service):
-    response = sandbox_service.open_sandbox_account()
+    response = sandbox_service.sandbox.open_sandbox_account()
     yield response.account_id
-    sandbox_service.close_sandbox_account(
+    sandbox_service.sandbox.close_sandbox_account(
         account_id=response.account_id,
     )
 
@@ -86,14 +86,14 @@ def order(figi, quantity, price, direction, account_id, order_type, order_id):
 )
 class TestSandboxOperations:
     def test_open_sandbox_account(self, sandbox_service):
-        response = sandbox_service.open_sandbox_account()
+        response = sandbox_service.sandbox.open_sandbox_account()
         assert isinstance(response.account_id, str)
-        sandbox_service.close_sandbox_account(
+        sandbox_service.sandbox.close_sandbox_account(
             account_id=response.account_id,
         )
 
     def test_get_sandbox_accounts(self, sandbox_service, account_id):
-        response = sandbox_service.get_sandbox_accounts()
+        response = sandbox_service.users.get_accounts()
         assert isinstance(response.accounts, list)
         assert isinstance(response.accounts[0], Account)
         assert (
@@ -108,8 +108,8 @@ class TestSandboxOperations:
         )
 
     def test_close_sandbox_account(self, sandbox_service):
-        response = sandbox_service.open_sandbox_account()
-        response = sandbox_service.close_sandbox_account(
+        response = sandbox_service.sandbox.open_sandbox_account()
+        response = sandbox_service.sandbox.close_sandbox_account(
             account_id=response.account_id,
         )
         assert isinstance(response, CloseSandboxAccountResponse)
@@ -117,23 +117,23 @@ class TestSandboxOperations:
     def test_post_sandbox_order(
         self, sandbox_service, order, figi, direction, quantity
     ):
-        response = sandbox_service.post_sandbox_order(**order)
+        response = sandbox_service.orders.post_order(**order)
         assert isinstance(response.order_id, str)
         assert response.figi == figi
         assert response.direction == direction
         assert response.lots_requested == quantity
 
     def test_get_sandbox_orders(self, sandbox_service, order, account_id):
-        _ = sandbox_service.post_sandbox_order(**order)
-        response = sandbox_service.get_sandbox_orders(
+        _ = sandbox_service.orders.post_order(**order)
+        response = sandbox_service.orders.get_orders(
             account_id=account_id,
         )
         assert isinstance(response.orders, list)
         assert len(response.orders) == 1
 
     def test_cancel_sandbox_order(self, sandbox_service, order, account_id):
-        response = sandbox_service.post_sandbox_order(**order)
-        response = sandbox_service.cancel_sandbox_order(
+        response = sandbox_service.orders.post_order(**order)
+        response = sandbox_service.orders.cancel_order(
             account_id=account_id,
             order_id=response.order_id,
         )
@@ -142,9 +142,9 @@ class TestSandboxOperations:
     def test_get_sandbox_order_state(
         self, sandbox_service, order, account_id, figi, direction, quantity
     ):
-        response = sandbox_service.post_sandbox_order(**order)
+        response = sandbox_service.orders.post_order(**order)
 
-        response = sandbox_service.get_sandbox_order_state(
+        response = sandbox_service.orders.get_order_state(
             account_id=account_id,
             order_id=response.order_id,
         )
@@ -154,15 +154,15 @@ class TestSandboxOperations:
 
     @pytest.mark.parametrize("order_type", [OrderType.ORDER_TYPE_MARKET])
     def test_get_sandbox_positions(self, sandbox_service, account_id, order):
-        _ = sandbox_service.post_sandbox_order(**order)
+        _ = sandbox_service.orders.post_order(**order)
 
-        response = sandbox_service.get_sandbox_positions(account_id=account_id)
+        response = sandbox_service.operations.get_positions(account_id=account_id)
 
         assert isinstance(response.money[0], MoneyValue)
         assert response.money[0].currency == "rub"
 
     def test_get_sandbox_operations(self, sandbox_service, account_id, order, figi):
-        response = sandbox_service.get_sandbox_operations(
+        response = sandbox_service.operations.get_operations(
             account_id=account_id,
             from_=datetime(2000, 2, 2),
             to=datetime(2022, 2, 2),
@@ -172,7 +172,7 @@ class TestSandboxOperations:
         assert isinstance(response.operations, list)
 
     def test_get_sandbox_portfolio(self, sandbox_service, account_id):
-        response = sandbox_service.get_sandbox_portfolio(
+        response = sandbox_service.operations.get_portfolio(
             account_id=account_id,
         )
         assert str(response.total_amount_bonds) == str(
@@ -194,7 +194,7 @@ class TestSandboxOperations:
     def test_sandbox_pay_in(self, sandbox_service, account_id):
         units_to_add = 10000
         nano_to_add = 100
-        response = sandbox_service.sandbox_pay_in(
+        response = sandbox_service.sandbox.sandbox_pay_in(
             account_id=account_id,
             amount=MoneyValue(currency="RUB", units=units_to_add, nano=nano_to_add),
         )
